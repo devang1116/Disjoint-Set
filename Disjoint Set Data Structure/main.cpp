02353f19b6c1d9fe99cf17626b5905f710615e4a
Devang <papinwardevang@gmail.com>
2021-10-05 08:25:54 +0530

    Initial Commit

---
+++ Disjoint Set Data Structure/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Disjoint Set Data Structure
+//
+//  Created by Devang Papinwar on 05/10/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
